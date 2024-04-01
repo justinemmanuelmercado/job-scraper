6e717bedf01f0c3211c23931451ab59751a8477e
Justin Emmanuel Mercado <ej@ejmercado.com>
2024-04-01 23:50:53 +0800

    Update hn ID

--- pkg/hackernews/hackernews.go
+++ pkg/hackernews/hackernews.go
@@ -31,8 +31,8 @@ type Parent struct {
 
 var (
 	hnSourceName = "HackerNews"
-	currentId    = 39562986 // Automate getting this
-	toGet        = 20       // Only need 20 I think
+	currentId    = 39894820 // Automate getting this
+	toGet        = 30       // Only need 20 I think
 )
 
 func extractTitle(text string) (string, string) {
