c0ae900d03b3768ffa22be0f3df82502f8e47c4e
Justin Emmanuel Mercado <ej@ejmercado.com>
2024-08-02 18:10:13 +0800

    Udpate for august

--- pkg/hackernews/hackernews.go
+++ pkg/hackernews/hackernews.go
@@ -31,8 +31,8 @@ type Parent struct {
 
 var (
 	hnSourceName = "HackerNews"
-	currentId    = 40846428 // Automate getting this
-	toGet        = 20       // Only need 20 I think
+	currentId    = 41129813 // Automate getting this
+	toGet        = 400      // Only need 20 I think
 )
 
 func extractTitle(text string) (string, string) {
