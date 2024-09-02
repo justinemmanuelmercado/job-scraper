cfd1a2c2e465524be111e951a57999e5f13355ad
Justin Emmanuel Mercado <ej@ejmercado.com>
2024-09-03 07:05:58 +0800

    Return to normal

--- pkg/hackernews/hackernews.go
+++ pkg/hackernews/hackernews.go
@@ -32,7 +32,7 @@ type Parent struct {
 var (
 	hnSourceName = "HackerNews"
 	currentId    = 41425910 // Automate getting this
-	toGet        = 300      // Only need 20 I think
+	toGet        = 30       // Only need 20 I think
 )
 
 func extractTitle(text string) (string, string) {
